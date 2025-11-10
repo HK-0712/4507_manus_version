b4832d9e90495ad216c90bd69ac5e3be4ef30349
HK0712 <dickson.chan05@gmail.com>
2025-11-10 18:05:20 +0800

    change name to Main

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TestEnsemble {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         EnsembleManager manager = new EnsembleManager();
